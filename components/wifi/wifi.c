cec12449e440769542e89e5c83b23a0d3216deee
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2025-05-30 23:29:23 +0200

    Set device name

--- components/wifi/wifi.c
+++ components/wifi/wifi.c
@@ -62,7 +62,8 @@ void wifi_init_sta(void) {
     s_wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    esp_netif_set_hostname(sta_netif, "Humidifier");
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
